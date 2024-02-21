c1f0d99251f18eda2d27ee07e1d186da8faa815c
pierre <kniazeff.pierre@hotmail.fr>
2024-02-21 11:27:34 +0100

    Removed database.php from repository

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// database.php
-return [
-    'database' => 'levelnepierre',
-    'host' => 'levelnepierre.mysql.db',
-    'user' => 'levelnepierre',
-    'password' => 'Moulinard95520'
-];
-
