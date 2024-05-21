2633b38362de5375743523fdaa9cf867aa1cca4c
pierre <kniazeff.pierre@hotmail.fr>
2024-05-21 15:29:22 +0200

    Update 7 du  21/05/24

--- views/common/MentionsLegales.php
+++ views/common/MentionsLegales.php
@@ -2,66 +2,115 @@
     <h1>Mentions légales</h1>
     <p>
     Informations légales <br>
-MENTIONS LEGALES : <br>
+MENTIONS LEGALES : <br><br>
 
-Correspondant aux obligations de l'article 6-III et 19 de la Loi numéro 2004-575 du 21 juin 2004 relative à la Confiance de l'économie numérique, dite L.C.E.N., nous informons les utilisateurs du site : levelnext.fr Les infos qui suivent :
-
-
-1. Déclaration légales :
-
-Propriétaire : Pierre Kniazeff
-Adresse du titulaire : 6 avenue du moulinard 95520 - Osny
-
-Le Concepteur du site internet : Kniazeff Pierre
-Décideur de publication est : Kniazeff Pierre
-Ou joindre le Responsable de la publication : kniazeff.pierre@hotmail.fr
-
-Le Développeur est : Kniazeff Pierre
-De quelle façon entrer en contact avec le Réalisateur : kniazeff.pierre@hotmail.fr
-
-Hebergeur du site web : OVH
-SAS au capital social de 10 174 560 Euros
-Immatriculation RCS Lille Métropole 424 761 419 00045
-Nomenclature APE 2620Z
-N*TVA : FR 22 424 761 419
-Siège social : 2 rue Kellermann - 59100 Roubaix - France
+Correspondant aux obligations de l'article 6-III et 19 de la Loi numéro 2004-575 du 21 juin 2004 
+relative à la Confiance de l'économie numérique, dite L.C.E.N., nous informons les utilisateurs 
+du site : levelnext.fr Les infos qui suivent :<br><br>
 
 
-Les mentions ont été réalisées via ce générateur
+1. Déclaration légales :<br><br>
 
-2. Introduction :
+Propriétaire : Pierre Kniazeff <br>
+Adresse du titulaire : 6 avenue du moulinard 95520 - Osny<br><br>
+
+Le Concepteur du site internet : Pierre Kniazeff <br>
+Décideur de publication est : Pierre Kniazeff <br>
+Ou joindre le Responsable de la publication : kniazeff.pierre@hotmail.fr <br><br>
+
+Le Développeur est : Pierre Kniazeff <br>
+De quelle façon entrer en contact avec le Réalisateur : kniazeff.pierre@hotmail.fr <br><br>
+
+Hebergeur du site web : OVH <br>
+SAS au capital social de 10 174 560 Euros <br>
+Immatriculation RCS Lille Métropole 424 761 419 00045 <br>
+Nomenclature APE 2620Z <br>
+N*TVA : FR 22 424 761 419 <br>
+Siège social : 2 rue Kellermann - 59100 Roubaix - France <br><br>
+
+
+Les mentions ont été réalisées via ce générateur <br><br>
+
+2. Introduction : <br><br>
 
 Tout internaute qui utilise le site suivant : levelnext.fr est considéré comme usageé du site suivant.
-Le site suivant levelnext.fr regroupe un ensemble de services, dans l'état, disponible pour les usageé. Il est ici précisé que ces derniers doivent rester honnête et faire preuve de bonne foi tant envers les autres usagés qu'envers le Webmaster du site levelnext.fr. Le site levelnext.fr est mis à jour de façon régulière par Kniazeff Pierre.
-Kniazeff Pierre s'efforce de prescrire sur le site internet levelnext.fr des informations les plus réalistes possibles (sous réserve de modifications), mais ne saurait attester l'exactitude, la finitude et l'actualité des informations répandu sur son site internet, qu'elles soient de son fait ou non. En répercussion, l'utilisateur reconnaît utiliser ces informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) sous sa responsabilité exclusive.
+Le site suivant levelnext.fr regroupe un ensemble de services, dans l'état, disponible pour les usagés. 
+Il est ici précisé que ces derniers doivent rester honnête et faire preuve de bonne foi tant envers les
+autres usagés qu'envers le Webmaster du site levelnext.fr. Le site levelnext.fr est mis à jour de façon 
+régulière par Pierre Kniazeff.
+Pierre Kniazeff s'efforce de prescrire sur le site internet levelnext.fr des informations les plus réalistes  
+possibles (sous réserve de modifications), mais ne saurait attester l'exactitude, la finitude et l'actualité 
+des informations répandu sur son site internet, qu'elles soient de son fait ou non. En répercussion, l'utilisateur 
+reconnaît utiliser ces informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) 
+sous sa responsabilité exclusive. <br><br>
+
+3. Exigence d'accessibilité : <br><br>
+
+Le site levelnext.fr est principalement accessible aux utilisateurs H24 7/7, sauf en cas de maintenance, 
+programmée ou pas, pour les besoins de sa maintenance ou force majeure. En cas de service indisponible, 
+levelnext.fr se devouera à faire son maximum pour rétablir l'accès au site suivant et s'efforcera alors
+d'informer au préalable aux utilisateurs Dates & heures de l'intervention. N'étant soumis qu'à une servitude 
+de moyen, levelnext.fr ne saurait être tenu comme responsable de tout dommage, peu importe sa nature, résultant
+d'une indisponibilité du service. <br><br>
 
 
-3. Exigence d'accessibilité :
+4. Droit de propriété intellectuelle : <br><br>
 
-Le site levelnext.fr est principalement accessible aux utilisateurs H24 7/7, sauf en cas de maintenance, programmée ou pas, pour les besoins de sa maintenance ou force majeure. En cas de service indisponible, levelnext.fr se devouera à faire son maximum pour rétablir l'accès au site suivant et s'efforcera alors d'informer au préalable aux utilisateurs Dates & heures de l'intervention. N'étant soumis qu'à une servitude de moyen, levelnext.fr ne saurait être tenu comme responsable de tout dommage, peu importe sa nature, résultant d'une indisponibilité du service
+Pierre Kniazeff est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits 
+et autorisations d'usage sur tous les éléments accessibles sur le site, tant structurellement que sur les textes,
+graphismes, icônes, sons, logiciels etc… 
+Toute reproduction totale ou partielle du site levelnext.fr, représentation, edition, mis à jour totalement 
+ou partiellement de l'un quelconque de ces éléments, peu importe le moyen ou la technique utilisé, 
+est formellement interdite, excepté acceptation écrite préalable de Pierre Kniazeff, responsable du site web 
+même partielle du site levelnext.fr, affichage, modification, adaptation totalement ou partiellement de l'un 
+de ces éléments, sans tenir compte du moyen ou le procédé utilisé, est interdite, sauf autorisation préalable 
+de Pierre Kniazeff, responsable du site internet à cette adresse : kniazeff.pierre@hotmail.fr, à contrario 
+elle sera considérée de la même manière qu'une contrefaçon et passible de poursuite selon les dispositions
+des différents articles L.335-2 et suivants du Code de Propriété Intellectuelle. <br><br>
 
 
-4. Droit de propriété intellectuelle :
+5. Liens hypertextes et cookies : <br><br>
 
-Kniazeff Pierre est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits et autorisations d'usage sur tous les éléments accessibles sur le site, tant structurellement que sur les textes, , graphismes, icônes, sons, logicielsetc…
-Toute reproduction totale ou partielle du site levelnext.fr, représentation, edition, mis à jour totalement ou partiellement de l'un quelconque de ces éléments, peu importe le moyen ou la technique utilisé, est formellementinterdite, excepté acceptation écrite préalable de Kniazeff Pierre, responsable du site web même partielle du site levelnext.fr, affichage, modification, adaptation totalement ou partiellement de l'un de ces éléments, sans tenir compte du moyen ou le procédé utilisé, est interdite, sauf autorisation préalable de Kniazeff Pierre, responsable du site internet à cette adresse : kniazeff.pierre@hotmail.fr, à contrario elle sera considérée de la même manière qu'une contrefaçon et passible de poursuite selon les dispositions des différents articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+Le site web levelnext.fr contient de multiple URL cliquable vers plusieurs sites (partenariats, sites sources .) 
+mis en place suite à l'accord de Pierre Kniazeff. Néanmoins, Pierre Kniazeff ne peut pas controler l'intégralité 
+du contenu des sites ainsi visités et rejette donc l'entière responsabilité de de cette façon concernant les 
+eventuels risques de contenus illicites. 
+L'usager est informé que lors de ses visites sur le site levelnext.fr, un ou différents cookies peuvent s'intégrer 
+automatiquement sur son pc en utilisant son logiciel informatique de navigation. Un cookie est un bloc de donnée 
+qui ne permet pas d'identifier l'utilisateur, cependant qui note des renseignements en lien avec la navigation 
+de l'utilisateur sur le site. 
+Le paramétrage du programme de navigation permet d'informer de la présence de cookie et éventuellement, 
+de pouvoir contester de la manière décrite à l' adresse suivante : www.levelnext.fr.
+L'utilisateur peut toutefois configurer le navigateur de son pc pour rejeter la mise en place des cookies, 
+étant conscient que le refus de la mise en place d'un cookie peut entraîner l'incapacité d'avoir accès à certains 
+secteurs. Pour tout blocage des cookies, tapez dans votre outil de recherche : blocage des cookies et
+appliquez les explications en fonction de votre version de navigateur. <br><br>
 
+6. Protection des biens et des personnes - Gestion des données personnelles : <br><br>
 
-5. Liens hypertextes et cookies :
+En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, 
+la loi n° 2004-801  du 6 août 2004, l'article L. 226-13 du Code pénal ainsi que la Directive Européenne 
+du 24 octobre 1995. 
+Sur le site levelnext.fr, Pierre Kniazeff ne collecte pas d'informations personnelles 
+(suivant l'article 4 loi n°78-17 du 06 janvier 1978) concernant l'usager que pour la nécéssité de certains 
+services offerts par le site levelnext.fr. L'utilisateur fournit ces données en pleine conscience de cause, 
+notamment quand il procède de lui-même à la saisie. Il est alors expliqué à l'usagé du site web levelnext.fr 
+la contrainte ou pas de fournir ces informations. 
+Conformément aux dispositifs des articles 38 et autres de la loi 78-17 du 6 janvier 1978 en lien avec 
+l'informatique, les fichiers et aux différentes libertés, n'importe quel utilisateur dispose d'un droit d'accès 
+aux informations, de rectification, de suppression et d'opposition à toutes les données personnelles 
+qui le concerne. Pour l'exercer, envoyez votre demande à levelnext.fr par mail : kniazeff.pierre@hotmail.fr 
+ou alors par écrit  signée, accompagnée d'une copie de pièce d'identité avec signature du propriétaire 
+du document, en mentionnant l'adresse à laquelle la réponse devra être envoyée. <br><br>
 
-Le site web levelnext.fr contient de multiple URL cliquable vers plusieurs sites (partenariats, sites sources .) mis en place suite à l'accord de Kniazeff Pierre. Néanmoins, Kniazeff Pierre ne peut pas controler l'intégralité du contenu des sites ainsi visités et rejette donc l'entière responsabilité de de cette façon concernant les eventuels risques de contenus illicites.
-L'usager est informé que lors de ses visites sur le site levelnext.fr, un ou différents cookies peuvent s'intégrer automatiquement sur son pc en utilisant son logiciel informatique de navigation. Un cookie est un bloc de donnée qui ne permet pas d'identifier l'utilisateur, cependant qui note des renseignements en lien avec la navigation de l'utilisateur sur le site. 
-Le paramétrage du programme de navigation permet d'informer de la présence de cookie et éventuellement, de pouvoir contester de la manière décrite à l' adresse suivante : www.levelnext.fr. La mise en place peut toutefois de configurer le navigateur de son pc pour rejeter la mise en place des cookies, étant conscient que le refus de la mise en place d'un cookie peut entraîner l'incapacité d'avoir accès à certains secteurs. Pour tout blocage des cookies, tapez dans votre outil de recherche : blocage des cookies et appliquez les explications en fonction de votre version de navigateur.
+Aucune information personnelle de l'usager du site levelnext.fr n'est publiée sans consentemment de l'utilisateur, 
+échangée, transmise, cédée sur n'importe quel support à des tiers. Uniquement l'hypothèse de vente 
+du site suivant levelnext.fr et de tout ses droits autorise Kniazeff Pierre à joindre les informations 
+à l' acquéreur qui serait alors lui-même tenu à la même obligation de conservation et de modification des données 
+relative à l'utilisateur du site levelnext.fr. 
+Le site interne levelnext.fr est conforme avec le RGPD consulter notre politique RGPD. <br><br>
 
-6. Protection des biens et des personnes - Gestion des données personnelles :
-
-En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978 , la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal ainsi que la Directive Européenne du 24 octobre 1995.
-Sur le site levelnext.fr, Kniazeff Pierre ne collecte pas d'informations personnelles ( suivant l'article 4 loi n°78-17 du 06 janvier 1978) concernant l'usager que pour la nécéssité de certains services offerts par le site levelnext.fr. L'utilisateur fournit ces données en pleine conscience de cause, notamment quand il procède de lui-même à la saisie. Il est alors expliqué à l'usagé du site web levelnext.fr la contrainte ou pas de fournir ces informations.
-Conformément aux dispositifs des articles 38 et autres de la loi 78-17 du 6 janvier 1978 en lien avec l'informatique, les fichiers et aux différentes libertés, n'importe quel utilisateur dispose d'un droit d'accès aux informations, de rectification, de suppression et d'opposition à toutes les données personnelles qui le concerne. Pour l'exercer, envoyez votre demande à levelnext.fr par mail : kniazeff.pierre@hotmail.fr ou alors par écrit  signée, accompagnée d'une copie de pièce d'identité avec signature du propriétaire du document, en mentionnant l'adresse à laquelle la réponse devra être envoyée.
-
-Aucune information personnelle de l'usager du site levelnext.fr n'est publiée sans consentemment de l'utilisateur, échangée, transmise, cédée sur n'importe quel support à des tiers. Uniquement l'hypothèse de vente du site suivant levelnext.fr et de tout ses droits autorise Kniazeff Pierre à joindre les informations à l' acquéreur qui serait alors lui-même tenu à la même obligation de conservation et de modification des données relative à l'utilisateur du site levelnext.fr.
-Le site interne levelnext.fr est conforme avec le RGPD consulter notre politique RGPD.
-
-Les bases de données sont protégées par les règles de la loi du 1er juillet 1998 transposant ainsi la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
+Les bases de données sont protégées par les règles de la loi du 1er juillet 1998 transposant ainsi
+la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données. <br>
     </p>
 </body>
