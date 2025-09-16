a768c25745adee122a80997a8f43afe76d12b41c
PierreKniazeff <150007638+PierreKniazeff@users.noreply.github.com>
2025-09-16 18:10:20 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 // database.php
 return [
-    'database' => 'levelnepierre',
-    'host' => 'levelnepierre.mysql.db',
-    'user' => 'levelnepierre',
-    'password' => 'Moulinard95520'
+    'database' => '',
+    'host' => '',
+    'user' => '',
+    'password' => ''
 ];
 
     
